aa8d4a5819819426d47b0e9435c91eef78f3c10f
Usman Pervaiz <de.usman@yahoo.com>
2017-03-30 11:52:01 -0400

    updated 30

--- bin/run.rb
+++ bin/run.rb
@@ -27,14 +27,54 @@ response = HTTParty.get(url)
 end
 
 
-def fetch_and_seed_models_for_ford_passenger_cars
-url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/460/vehicleType/passenger_car?format=json"
-response = HTTParty.get(url)
-	response["Results"].each do |car|
-        Make.find_or_create_by(name: car["MakeName"].downcase)
-        end
+def fetch_and_seed_models
+	new_array = []
+	make_ids = [460, 474, 475, 515, 452, 449, 469, 442, 448, 478]
+	make_id = 0
+	Make.all.each do |make|
+		Type.all.each do |type|
+			type = type.name.gsub(" ", "_")
+			
+			if (make.name == "ford")
+				make_id = 460
+			elsif (make.name == "honda")
+				make_id = 474
+			elsif (make.name == "acura")
+				make_id = 475
+			elsif (make.name == "lexus")
+				make_id = 515
+			elsif (make.name == "bmw")
+				make_id = 452
+			elsif (make.name == "mercedes-benz")
+				make_id = 449
+			elsif(make.name == "cadillac")
+				make_id = 469
+			elsif (make.name == "jaguar")
+				make_id = 442
+			elsif (make.name == "toyota")
+				make_id = 448
+			elsif (make.name == "nissan")
+				make_id = 478
+			end
 
+
+			url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/#{make_id}/vehicleType/#{type}?format=json"
+			response = HTTParty.get(url)
+				response["Results"].each do |car|
+					binding.pry
+				model = Model.find_or_create_by(car["Model_Name"])
+				model.make = make.id
+				model.type = type.id
+				model.save
+				end
+			
+		end
 	end
+	
 end
+
+fetch_and_seed_vehicle_type
+fetch_and_seed_makes
+ 
 
 
--- db/migrate/20170329202158_create_models.rb
+++ db/migrate/20170329202158_create_models.rb
@@ -2,7 +2,7 @@ class CreateModels < ActiveRecord::Migration[5.0]
   def change
    	create_table :models do |t|
   		t.string :name
-  		t.integer :brand_id
+  		t.integer :make_id
   		t.integer :type_id
   	end
   end
